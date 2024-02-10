db9f4e96faf2e00627c25f1e5a1820d553bf3881
Sandeep <sandeepsharma999000@gmail.com>
2024-02-10 12:04:28 +0530

    ss

--- src/TS.tsx
+++ src/TS.tsx
@@ -7,83 +7,119 @@ import {
   type MRT_RowVirtualizer,
 } from 'material-react-table';
 import { makeData, type Person } from './makeData';
-
+import { Box } from '@mui/material';
+import  data1  from "./json.json";
 const Example = () => {
+
+
+function working(s: string){
+
+  // let sm:any = s.split(', ');
+  //   let smt =  sm?.[0];
+
+if(s){
+  let sm:any = s.split(',');
+  let a =  sm;
+  // console.log(sm?.[0]);
+
+  for (let index = 0; index < a.length; index++) {
+    let element = a[index];
+    return (
+      <>
+        {element}
+      </>
+    );
+  }
+
+
+ 
+  // return   sm[0];
+}
+
+
+
+  // if (smt){
+  //   return smt;
+  // }else{
+  //   return s;
+  // }
+  // }else{
+  //   return s;
+  // }
+  // Display output 
+//  console.log(s);
+  return s;
+}
+
   const columns = useMemo<MRT_ColumnDef<Person>[]>(
     //column definitions...
     () => [
       {
-        accessorKey: 'firstName',
-        header: 'First Name',
-        size: 150,
+        accessorKey: 'post_title',
+        header: 'Name',
+        filterVariant: 'text', // default
+        // size: 150,
       },
       {
-        accessorKey: 'middleName',
-        header: 'Middle Name',
-        size: 170,
+        accessorKey: 'field_phone',
+        header: 'Phone',
+        filterVariant: 'text', // default
+        // size: 150,
       },
       {
-        accessorKey: 'lastName',
-        header: 'Last Name',
-        size: 150,
+        accessorKey: 'field_fax',
+        header: 'Fax',
+        filterVariant: 'text', // default
+        // size: 150,
       },
       {
-        accessorKey: 'email',
-        header: 'Email Address',
-        size: 300,
+        accessorKey: 'field_website',
+        header: 'URL',
+        filterVariant: 'text', // default
+        // size: 150,
       },
       {
-        accessorKey: 'phoneNumber',
-        header: 'Phone Number',
-        size: 250,
+        accessorKey: 'category',
+        // filterVariant: 'range', //if not using filter modes feature, use this instead of filterFn
+        // filterFn: 'between',
+        filterVariant: 'text', // default
+        header: 'Category',
+        size: 200,
+        //custom conditional format and styling
+        Cell: ({ cell }) => (
+          
+          <>
+            <Box
+              component="span"
+              sx={(theme) => ({
+                backgroundColor:
+                  cell.getValue<number>() < 50_000
+                    ? theme.palette.error.dark
+                    : cell.getValue<number>() >= 50_000 &&
+                      cell.getValue<number>() < 75_000
+                      ? theme.palette.warning.dark
+                      : theme.palette.success.dark,
+                borderRadius: '0.25rem',
+                color: '#fff',
+                maxWidth: '9ch',
+                p: '0.25rem',
+              })}
+            >
+              {/* {cell.getValue<string>()} */}
+              {/* <dd>{recipe.ingredient.join(",")}</dd> */}
+
+              {
+                working(cell.getValue<string>())
+              }
+            
+
+            </Box>
+           
+
+          </>
+        ),
       },
-      {
-        accessorKey: 'address',
-        header: 'Address',
-        size: 300,
-      },
-      {
-        accessorKey: 'zipCode',
-        header: 'Zip Code',
-      },
-      {
-        accessorKey: 'city',
-        header: 'City',
-        size: 220,
-      },
-      {
-        accessorKey: 'state',
-        header: 'State',
-      },
-      {
-        accessorKey: 'country',
-        header: 'Country',
-        size: 350,
-      },
-      {
-        accessorKey: 'petName',
-        header: 'Pet Name',
-      },
-      {
-        accessorKey: 'age',
-        header: 'Age',
-      },
-      {
-        accessorKey: 'salary',
-        header: 'Salary',
-      },
-      {
-        accessorKey: 'dateOfBirth',
-        header: 'Date of Birth',
-      },
-      {
-        accessorKey: 'dateOfJoining',
-        header: 'Date of Joining',
-      },
-      {
-        accessorKey: 'isActive',
-        header: 'Is Active',
-      },
+     
     ],
     [],
     //end
@@ -92,13 +128,14 @@ const Example = () => {
   //optionally access the underlying virtualizer instance
   const rowVirtualizerInstanceRef = useRef<MRT_RowVirtualizer>(null);
 
-  const [data, setData] = useState<Person[]>([]);
+  const [data, setData] = useState([] as any);
   const [isLoading, setIsLoading] = useState(true);
   const [sorting, setSorting] = useState<MRT_SortingState>([]);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      setData(makeData(10_000));
+      // setData(makeData(10000));
+      setData(data1);
       setIsLoading(false);
     }
   }, []);
@@ -124,13 +161,14 @@ const Example = () => {
     enableColumnPinning: true,
     enableRowNumbers: true,
     enableRowVirtualization: true,
+    // initialState: { showColumnFilters: true },
     // muiTableContainerProps: { sx: { maxHeight: '600px' } },
     onSortingChange: setSorting,
     state: { isLoading, sorting },
     rowVirtualizerInstanceRef, //optional
     rowVirtualizerOptions: { overscan: 5 }, //optionally customize the row virtualizer
     columnVirtualizerOptions: { overscan: 2 }, //optionally customize the column virtualizer
-    initialState: { pagination: { pageSize: 14, pageIndex: 0 } },
+    initialState: { pagination: { pageSize: 100, pageIndex: 0 }, showColumnFilters: true },
     muiPaginationProps: {
       color: 'primary',
       shape: 'rounded',
